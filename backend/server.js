d5ac4a4a8073a40c5845182cc56f01ef55468eb3
hari <hari.harmalkar@gmail.com>
2018-03-30 19:03:21 +0530

    server side validation and return error

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ app.get('/api/games',(req,res) => {
 	db.collection('games').find({}).toArray((err,games) => {
 		res.json({games});
 	});
-})
+});
+
+app.use((req,res) => {
+	res.status(404).json({
+		errors:{
+			global:"still working on it.."
+		}
+	})
+});
 
 app.listen(8080, () => console.log('Server is running on localhost:8080'));
 
--- src/GameForm.js
+++ src/GameForm.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import {connect} from 'react-redux';
+import {saveGame} from './actions'
 
 class GameForm extends Component {
 state ={
 	title:'',
 	cover:'',
-	errors:{}
+	errors:{},
+	loading:false
 }
 handleChange = (e) => {
+
 	if(!!this.state.errors[e.target.name]){
 	let errors = Object.assign({},this.state.errors);
 	delete errors[e.target.name];
 	this.setState({[e.target.name]:e.target.value,
 		errors});
+
 	}else{
-	this.setState({[e.target.name]:e.target.value});
+		this.setState({[e.target.name]:e.target.value});
 	}
 	
 }
@@ -26,13 +31,23 @@ handleSubmit = (e) => {
 	if(this.state.title === '') errors.title = "Can't be empty";
 	if(this.state.cover === '') errors.cover = "Can't be empty";
 	this.setState({errors})
+	const isValid = Object.keys(errors).length === 0;
+	if(isValid){
+		const {title,cover} = this.state;
+		this.setState({loading:true});
+		this.props.saveGame({title,cover}).then(
+			() => {	console.log('hi');		},
+			(err) => {err.response.json().then(({errors}) => this.setState({errors,loading:false}))}
+		);
+
+	}
 }
 
     render() {
         return (
-        <form className="ui form" onSubmit={this.handleSubmit}>
+        <form className={classnames('ui','form',{loading:this.state.loading})} onSubmit={this.handleSubmit}>
          	<h1>Add new game</h1>
-
+			{!!this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div>}
          	<div className="field" className={classnames('field',{error: !!this.state.errors.title})}>
          		<label htmlFor="title">Title</label>
          		<input 
@@ -62,4 +77,4 @@ handleSubmit = (e) => {
     }
 }
 
-export default GameForm;
+export default connect(null,{saveGame})(GameForm);
