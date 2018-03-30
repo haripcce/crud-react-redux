3f127a368ae416b84e3c4783a68cf4ce0f779f00
hari <hari.harmalkar@gmail.com>
2018-03-30 14:15:46 +0530

    dispaly empty message

--- src/GamesPage.js
+++ src/GamesPage.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import GamesList from './GamesList';
+import PropTypes from 'prop-types';
 
 class GamesPage extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Games List</h1>
+				<GamesList  games={this.props.games}/>
 			</div>
 		);
 	}
 }
 
-export default GamesPage;
+GamesPage.propTypes = {
+  games  : PropTypes.array.isRequired
+}
+
+function mapStateToProps(state){
+return{
+	games:state.games
+}
+}
+export default connect(mapStateToProps)(GamesPage);
