f42caa15b935e610fb5421fac2ae21f0b74707fa
hari <hari.harmalkar@gmail.com>
2018-03-30 12:29:37 +0530

    initial commit with redux installed

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(thunk))
+	);
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>  , document.getElementById('root'));
+registerServiceWorker();
