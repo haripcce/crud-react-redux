92a6e98fdca8daba85f63f3935b6ff1fa457bf34
hari <hari.harmalkar@gmail.com>
2018-03-30 17:57:10 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 //import {Match} from 'react-router';
-import { Link, Switch,Route} from 'react-router-dom';
+import { NavLink, Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import GamesPage from './GamesPage';
-
+import GameForm from './GameForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <Link to="/games" >Games</Link>
-
-        </p>
-        <Switch>
-        
-        <Route path="/games" component={GamesPage} />
        
-      </Switch>
+       <div className="ui container">
+         <div className="ui three item menu">
+           <NavLink className="item" activeClassName="active" exact to="/">Home</NavLink>
+           <NavLink className="item" activeClassName="active" exact to="/games">Games</NavLink>
+           <NavLink className="item" activeClassName="active" exact to="/games/new">Add New Game</NavLink>
+         </div>  
+       </div>
+       
+        <Switch>
+          <Route path="/games/new" component={GameForm} />
+          <Route path="/games" component={GamesPage} />
+        </Switch>
       </div>
     );
   }
--- src/GamesPage.js
+++ src/GamesPage.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import GamesList from './GamesList';
 import PropTypes from 'prop-types';
+import {fetchGames} from './actions'
 
 class GamesPage extends Component {
+
+	componentDidMount() {
+		this.props.fetchGames();
+	}
+
+
 	render() {
 		return (
 			<div>
@@ -15,12 +22,12 @@ class GamesPage extends Component {
 }
 
 GamesPage.propTypes = {
-  games  : PropTypes.array.isRequired
+  games  : PropTypes.array.isRequired,
+  fetchGames: PropTypes.func.isRequired
 }
-
 function mapStateToProps(state){
 return{
 	games:state.games
 }
 }
-export default connect(mapStateToProps)(GamesPage);
+export default connect(mapStateToProps,{fetchGames})(GamesPage);
